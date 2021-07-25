f3b69ae63c6bb2982e788649f54721578e28316d
Shikha Suman <shikhasuman4112001@gmail.com>
2021-07-25 11:06:42 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Note from "./Note";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function App(){
+    return <div><Header />
+    <Note />
+    <Footer /></div>;
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Footer(){
+    const currentYear = new Date().getFullYear();
+    return <footer><p>&copy; {currentYear}</p></footer>;
+}
+
+export default Footer;
